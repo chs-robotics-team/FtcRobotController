f5a6fa206f95f25f4c4319c174c8e7098c7ab202
DuroCodes <davidwright13503@gmail.com>
2023-09-14 10:31:36 -0400

    feat: add and test kotlin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestKotlin.kt
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
+@TeleOp(name = "test")
+class TestKotlin : OpMode() {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+    override fun loop() {
+        TODO("Not yet implemented")
+    }
+}
