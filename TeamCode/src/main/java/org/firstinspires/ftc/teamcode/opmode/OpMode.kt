319ee1cfea4c6516750fe6834cb1d28ba1d2cdd4
DuroCodes <davidwright13503@gmail.com>
2024-02-07 14:45:04 -0500

    feat: add wrist logic hopefully

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/OpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/OpMode.kt
@@ -13,9 +13,11 @@ private const val ARM_SPEED = 0.5
 private const val MAX_ARM_POSITION = 180.0
 private const val DRIVE_SPEED = 0.4
 private const val SLIDE_SPEED = 0.075
-private const val MAX_SLIDE_POSITION = 180.0
-private const val CLAW_OPEN_POSITION = 0.0
-private const val CLAW_CLOSE_POSITION = 180.0
+private const val MAX_SLIDE_POS = 180.0
+private const val CLAW_OPEN_POS = 0.0
+private const val CLAW_CLOSE_POS = 180.0
+private const val CLAW_WRIST_UP_POS = 0.0
+private const val CLAW_WRIST_DOWN_POS = 180.0
 
 @TeleOp(name = "OpMode")
 @Suppress("unused")
@@ -23,16 +25,19 @@ class OpMode : OpMode() {
     private lateinit var leftSlideMotor: Motor
     private lateinit var rightSlideMotor: Motor
     private lateinit var clawArm: Motor
+    private lateinit var clawWrist: SimpleServo
     private lateinit var clawServo: SimpleServo
     private lateinit var driveTrain: MecanumDrive
     private lateinit var gamepad: GamepadEx
     private lateinit var clawOpen: ToggleButtonReader
+    private lateinit var wristBack: ToggleButtonReader
 
     override fun init() {
         leftSlideMotor = Motor(hardwareMap, "lSlide")
         rightSlideMotor = Motor(hardwareMap, "rSlide")
         clawArm = Motor(hardwareMap, "clawMotor")
-        clawServo = SimpleServo(hardwareMap, "clawServo", CLAW_OPEN_POSITION, CLAW_CLOSE_POSITION)
+        clawWrist = SimpleServo(hardwareMap, "clawWrist", CLAW_WRIST_UP_POS, CLAW_WRIST_DOWN_POS)
+        clawServo = SimpleServo(hardwareMap, "clawServo", CLAW_OPEN_POS, CLAW_CLOSE_POS)
 
         driveTrain = MecanumDrive(
             Motor(hardwareMap, "flMotor"),
@@ -43,6 +48,8 @@ class OpMode : OpMode() {
 
         gamepad = GamepadEx(gamepad1)
         clawOpen = ToggleButtonReader(gamepad, GamepadKeys.Button.A)
+        // TODO: change this to use the rotation of the arm to determine if the wrist is back
+        wristBack = ToggleButtonReader(gamepad, GamepadKeys.Button.B)
 
         telemetry.addData("[BOT]", "Initialized Motors")
     }
@@ -61,7 +68,7 @@ class OpMode : OpMode() {
 
         val avgSlidePos = (leftSlideMotor.currentPosition + rightSlideMotor.currentPosition) / 2.0
 
-        val slidePower = if (avgSlidePos < MAX_SLIDE_POSITION) when {
+        val slidePower = if (avgSlidePos < MAX_SLIDE_POS) when {
             gamepad.isDown(GamepadKeys.Button.DPAD_UP) -> SLIDE_SPEED
             gamepad.isDown(GamepadKeys.Button.DPAD_DOWN) -> -SLIDE_SPEED
             else -> 0.0
@@ -73,11 +80,14 @@ class OpMode : OpMode() {
             else -> 0.0
         } else 0.0
 
-        val clawPosition = if (clawOpen.state) CLAW_OPEN_POSITION else CLAW_CLOSE_POSITION
+        // TODO: change this to use the rotation of the arm to determine the wrist position
+        val wristPosition = if (wristBack.state) CLAW_WRIST_DOWN_POS else CLAW_WRIST_UP_POS
+        val clawPosition = if (clawOpen.state) CLAW_OPEN_POS else CLAW_CLOSE_POS
 
         leftSlideMotor.set(slidePower)
         rightSlideMotor.set(slidePower)
         clawArm.set(armPower)
         clawServo.position = clawPosition
+        clawWrist.position = wristPosition
     }
 }
