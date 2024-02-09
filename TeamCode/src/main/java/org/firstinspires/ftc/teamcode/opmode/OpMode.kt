dbb14c6a72c14a56587daf48cceadadd99c35294
DuroCodes <davidwright13503@gmail.com>
2024-02-09 09:16:47 -0500

    refactor: move constants & hardware into separate file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/OpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/OpMode.kt
@@ -1,93 +1,45 @@
 package org.firstinspires.ftc.teamcode.opmode
 
-import com.arcrobotics.ftclib.drivebase.MecanumDrive
 import com.arcrobotics.ftclib.gamepad.GamepadEx
 import com.arcrobotics.ftclib.gamepad.GamepadKeys
-import com.arcrobotics.ftclib.gamepad.ToggleButtonReader
-import com.arcrobotics.ftclib.hardware.SimpleServo
-import com.arcrobotics.ftclib.hardware.motors.Motor
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
-
-private const val ARM_SPEED = 0.5
-private const val MAX_ARM_POSITION = 180.0
-private const val DRIVE_SPEED = 0.4
-private const val SLIDE_SPEED = 0.075
-private const val MAX_SLIDE_POS = 180.0
-private const val CLAW_OPEN_POS = 0.0
-private const val CLAW_CLOSE_POS = 180.0
-private const val CLAW_WRIST_UP_POS = 0.0
-private const val CLAW_WRIST_DOWN_POS = 180.0
+import org.firstinspires.ftc.teamcode.util.Constants
+import org.firstinspires.ftc.teamcode.util.RobotHardware
 
 @TeleOp(name = "OpMode")
 @Suppress("unused")
 class OpMode : OpMode() {
-    private lateinit var leftSlideMotor: Motor
-    private lateinit var rightSlideMotor: Motor
-    private lateinit var clawArm: Motor
-    private lateinit var clawWrist: SimpleServo
-    private lateinit var clawServo: SimpleServo
-    private lateinit var driveTrain: MecanumDrive
     private lateinit var gamepad: GamepadEx
-    private lateinit var clawOpen: ToggleButtonReader
-    private lateinit var wristBack: ToggleButtonReader
+    private lateinit var hardware: RobotHardware
 
     override fun init() {
-        leftSlideMotor = Motor(hardwareMap, "lSlide")
-        rightSlideMotor = Motor(hardwareMap, "rSlide")
-        clawArm = Motor(hardwareMap, "clawMotor")
-        clawWrist = SimpleServo(hardwareMap, "clawWrist", CLAW_WRIST_UP_POS, CLAW_WRIST_DOWN_POS)
-        clawServo = SimpleServo(hardwareMap, "clawServo", CLAW_OPEN_POS, CLAW_CLOSE_POS)
-
-        driveTrain = MecanumDrive(
-            Motor(hardwareMap, "flMotor"),
-            Motor(hardwareMap, "frMotor"),
-            Motor(hardwareMap, "blMotor"),
-            Motor(hardwareMap, "brMotor")
-        )
-
         gamepad = GamepadEx(gamepad1)
-        clawOpen = ToggleButtonReader(gamepad, GamepadKeys.Button.A)
-        // TODO: change this to use the rotation of the arm to determine if the wrist is back
-        wristBack = ToggleButtonReader(gamepad, GamepadKeys.Button.B)
+        hardware = RobotHardware(hardwareMap, gamepad)
 
-        telemetry.addData("[BOT]", "Initialized Motors")
+        telemetry.addData("[BOT]", "Initialized Hardware & Gamepad")
     }
 
     override fun start() {
-        telemetry.addData("[BOT]", "Started")
+        telemetry.addData("[BOT]", "Started OpMode")
     }
 
     override fun loop() {
-        driveTrain.driveRobotCentric(
-            gamepad.leftX * DRIVE_SPEED,
-            gamepad.leftY * DRIVE_SPEED,
-            gamepad.rightX * DRIVE_SPEED,
-            false
+        hardware.toggleA.readValue()
+        telemetry.addData("A", hardware.toggleA.state)
+        hardware.toggleB.readValue()
+        telemetry.addData("B", hardware.toggleB.state)
+        telemetry.addData("Bumper", gamepad.isDown(GamepadKeys.Button.RIGHT_BUMPER))
+        telemetry.addData("Encoder", hardware.armMotor.encoder.position)
+
+        hardware.mecanumDrive.driveRobotCentric(
+            gamepad.leftX * Constants.DriveTrain.DRIVE_SPEED,
+            gamepad.leftY * Constants.DriveTrain.DRIVE_SPEED,
+            -gamepad.rightX * Constants.DriveTrain.DRIVE_SPEED,
         )
 
-        val avgSlidePos = (leftSlideMotor.currentPosition + rightSlideMotor.currentPosition) / 2.0
-
-        val slidePower = if (avgSlidePos < MAX_SLIDE_POS) when {
-            gamepad.isDown(GamepadKeys.Button.DPAD_UP) -> SLIDE_SPEED
-            gamepad.isDown(GamepadKeys.Button.DPAD_DOWN) -> -SLIDE_SPEED
-            else -> 0.0
-        } else 0.0
-
-        val armPower = if (clawArm.currentPosition < MAX_ARM_POSITION) when {
-            gamepad.isDown(GamepadKeys.Button.LEFT_BUMPER) -> ARM_SPEED
-            gamepad.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.4 -> -ARM_SPEED
-            else -> 0.0
-        } else 0.0
-
-        // TODO: change this to use the rotation of the arm to determine the wrist position
-        val wristPosition = if (wristBack.state) CLAW_WRIST_DOWN_POS else CLAW_WRIST_UP_POS
-        val clawPosition = if (clawOpen.state) CLAW_OPEN_POS else CLAW_CLOSE_POS
-
-        leftSlideMotor.set(slidePower)
-        rightSlideMotor.set(slidePower)
-        clawArm.set(armPower)
-        clawServo.position = clawPosition
-        clawWrist.position = wristPosition
+        hardware.clawArm.move()
+//        hardware.slide.move()
+        gamepad.readButtons()
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotHardware.kt
@@ -0,0 +1,152 @@
+package org.firstinspires.ftc.teamcode.util
+
+import com.arcrobotics.ftclib.drivebase.MecanumDrive
+import com.arcrobotics.ftclib.gamepad.GamepadEx
+import com.arcrobotics.ftclib.gamepad.GamepadKeys
+import com.arcrobotics.ftclib.gamepad.ToggleButtonReader
+import com.arcrobotics.ftclib.hardware.SimpleServo
+import com.arcrobotics.ftclib.hardware.motors.Motor
+import com.qualcomm.robotcore.hardware.HardwareMap
+
+object Constants {
+    object Arm {
+        const val SPEED = 0.4
+        const val UP_POS = 90
+
+        // The max position of the arm so it doesn't hit the back acrylic plate
+        const val MAX_POSITION = 180.0
+    }
+
+    object DriveTrain {
+        const val DRIVE_SPEED = 0.4
+    }
+
+    object Claw {
+        const val OPEN_POS = 0.0
+        const val CLOSE_POS = 90.0
+
+        // Position when arm is placing pixel on the board
+        const val WRIST_UP_POS = 90.0
+
+        // Position when arm is on the ground
+        const val WRIST_DOWN_POS = 0.0
+    }
+
+//    object Slide {
+//        const val SPEED = 0.075
+//        const val MAX_POSITION = 180.0
+//        const val MIN_POSITION = 0.0
+//    }
+}
+
+// TODO: finish this when we get slide motor connectors
+//data class Slide(val hardware: RobotHardware) {
+//    private val (left, right) = hardware.leftSlide to hardware.rightSlide
+//    private val motors = listOf(left, right)
+//
+//    init {
+//        motors.forEach { it.setRunMode(Motor.RunMode.RawPower) }
+//    }
+//
+//    fun move() {
+//        val (dpadUp, dpadDown) = gamepad.getButton(GamepadKeys.Button.DPAD_UP) to hardware.gamepad.getButton(
+//            GamepadKeys.Button.DPAD_DOWN
+//        )
+//
+//        val avgSlidePos = (left.currentPosition + right.currentPosition) / 2.0
+//
+//        val power = when {
+//            dpadUp && avgSlidePos < Constants.Slide.MAX_POSITION -> Constants.Slide.SPEED
+//            dpadDown && avgSlidePos > Constants.Slide.MIN_POSITION -> -Constants.Slide.SPEED
+//            else -> 0.0
+//        }
+//
+//        motors.forEach { it.set(power) }
+//    }
+//}
+
+class ClawArm(val hardware: RobotHardware) {
+    init {
+        hardware.armMotor.setRunMode(Motor.RunMode.PositionControl)
+        hardware.armMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE)
+
+        hardware.clawServo.position = Constants.Claw.OPEN_POS
+        hardware.wristServo.position = Constants.Claw.WRIST_DOWN_POS
+    }
+
+    fun move() {
+        val (leftBumper, leftTrigger) = hardware.gamepad.getButton(GamepadKeys.Button.LEFT_BUMPER) to hardware.gamepad.getTrigger(
+            GamepadKeys.Trigger.LEFT_TRIGGER
+        )
+
+        val armPower = if (hardware.armMotor.currentPosition < Constants.Arm.MAX_POSITION) when {
+            leftBumper -> Constants.Arm.SPEED
+            leftTrigger > 0.4 -> -Constants.Arm.SPEED
+            else -> 0.0
+        } else 0.0
+
+        // wrist position based on encoder of arm (if arm is up, wrist is up)
+        val wristPosition = when {
+            hardware.armMotor.currentPosition < Constants.Arm.UP_POS -> Constants.Claw.WRIST_DOWN_POS
+            else -> Constants.Claw.WRIST_UP_POS
+        }
+
+        hardware.armMotor.set(armPower)
+        hardware.wristServo.position = wristPosition
+    }
+}
+
+// Alternative for FTCLib MecanumDrive if we want to use our own
+//data class DriveTrain(
+//    val frontLeft: Motor,
+//    val frontRight: Motor,
+//    val backLeft: Motor,
+//    val backRight: Motor,
+//    val gamepad: GamepadEx,
+//    val speed: Double = Constants.DriveTrain.DRIVE_SPEED,
+//) {
+//    private val motors = listOf(frontLeft, frontRight, backLeft, backRight)
+//
+//    init {
+//        motors.forEach { it.setRunMode(Motor.RunMode.RawPower) }
+//    }
+//
+//    fun drive() {
+//        val (leftX, leftY, rightX) = Triple(gamepad.leftX, gamepad.leftY, gamepad.rightX)
+//
+//        // Maximum value of the joystick inputs; keeps motors ∈ [-1, 1]
+//        val denominator = (abs(leftX) + abs(leftY) + abs(rightX)).coerceAtLeast(1.0)
+//
+//        frontLeft.set((leftY + leftX + rightX) / denominator * speed)
+//        frontRight.set((leftY - leftX - rightX) / denominator * speed)
+//        backLeft.set((leftY - leftX + rightX) / denominator * speed)
+//        backRight.set((leftY + leftX - rightX) / denominator * speed)
+//    }
+//}
+
+class RobotHardware(val hardwareMap: HardwareMap, val gamepad: GamepadEx) {
+    private val leftFrontMotor = Motor(hardwareMap, "flMotor", Motor.GoBILDA.RPM_435)
+    private val rightFrontMotor = Motor(hardwareMap, "frMotor", Motor.GoBILDA.RPM_435)
+    private val leftBackMotor = Motor(hardwareMap, "blMotor", Motor.GoBILDA.RPM_435)
+    private val rightBackMotor = Motor(hardwareMap, "brMotor", Motor.GoBILDA.RPM_435)
+
+    val armMotor = Motor(hardwareMap, "armMotor", Motor.GoBILDA.RPM_435)
+    val clawServo = SimpleServo(
+        hardwareMap, "clawServo", Constants.Claw.WRIST_UP_POS, Constants.Claw.WRIST_DOWN_POS
+    )
+    val wristServo = SimpleServo(
+        hardwareMap, "pivotServo", Constants.Claw.OPEN_POS, Constants.Claw.CLOSE_POS
+    )
+
+    val mecanumDrive = MecanumDrive(leftFrontMotor, rightFrontMotor, leftBackMotor, rightBackMotor)
+    val clawArm = ClawArm(this)
+//    val slide = Slide(this)
+
+    val toggleA = ToggleButtonReader(gamepad, GamepadKeys.Button.A)
+    val toggleB = ToggleButtonReader(gamepad, GamepadKeys.Button.B)
+
+    init {
+        armMotor.setRunMode(Motor.RunMode.PositionControl)
+        armMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE)
+    }
+}
